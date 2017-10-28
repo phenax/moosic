fb8038621b6b2bb774e093a0ccd468a2a07db715
Akshay Nair <phenax5@gmail.com>
2017-10-28 15:30:51 +0530

    Added html element shorthands

--- src/main/kotlin/view/AbstractView.kt
+++ src/main/kotlin/view/AbstractView.kt
@@ -6,7 +6,7 @@ abstract class Node {
 }
 
 
-class HtmlNode(val nodeName: String, val props: Map<String, String>?, val children: List<Node>?): Node() {
+class HtmlNode(val nodeName: String, val props: Map<String, String>? = null, val children: List<Node>? = null): Node() {
 
 	override fun toHtml(): String {
 
@@ -49,25 +49,16 @@ abstract class AbstractView {
 		return TextNode(text)
 	}
 
-	// fun html(props?: Map<String, String>, children?: List<Node>) { return h("html", props, children) }
-	// fun body(props?: Map<String, String>, children?: List<Node>) { return h("body", props, children) }
-	// fun head(props?: Map<String, String>, children?: List<Node>) { return h("head", props, children) }
-	// fun div(props?: Map<String, String>, children?: List<Node>) { return h("div", props, children) }
-	// fun h1(props?: Map<String, String>, children?: List<Node>) { return h("h1", props, children) }
-	// fun h2(props?: Map<String, String>, children?: List<Node>) { return h("h2", props, children) }
-	// fun h3(props?: Map<String, String>, children?: List<Node>) { return h("h3", props, children) }
-	// fun h4(props?: Map<String, String>, children?: List<Node>) { return h("h4", props, children) }
-	// fun h5(props?: Map<String, String>, children?: List<Node>) { return h("h5", props, children) }
-	// fun h6(props?: Map<String, String>, children?: List<Node>) { return h("h6", props, children) }
-
-	// fun script(src: String, async: Boolean = true, defer: Boolean = true) {
-	// 	val props = MutableMap()
-
-	// 	props.put("src", src)
-
-	// 	if(async) props.put("async", "async")
-	// 	if(defer) props.put("defer", "defer")
-
-	// 	return h("script", props)
-	// }
+	// Html element shorthands
+	fun html(props: Map<String, String>?, children: List<Node>?): Node { return h("html", props, children) }
+	fun body(props: Map<String, String>?, children: List<Node>?): Node { return h("body", props, children) }
+	fun head(props: Map<String, String>?, children: List<Node>?): Node { return h("head", props, children) }
+	fun div(props: Map<String, String>?, children: List<Node>?): Node { return h("div", props, children) }
+	fun h1(props: Map<String, String>?, children: List<Node>?): Node { return h("h1", props, children) }
+	fun h2(props: Map<String, String>?, children: List<Node>?): Node { return h("h2", props, children) }
+	fun h3(props: Map<String, String>?, children: List<Node>?): Node { return h("h3", props, children) }
+	fun h4(props: Map<String, String>?, children: List<Node>?): Node { return h("h4", props, children) }
+	fun h5(props: Map<String, String>?, children: List<Node>?): Node { return h("h5", props, children) }
+	fun h6(props: Map<String, String>?, children: List<Node>?): Node { return h("h6", props, children) }
+	fun script(src: String, props: Map<String, String>?): Node { return h("script", props, null) }
 }
